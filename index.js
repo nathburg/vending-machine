ce81757f1b6bd044e544b43cdb603952a784c293
Nathan Burgess <nathans.email.address@gmail.com>
2023-01-24 13:39:27 -0800

    refactored so coins are an array of objects

--- index.js
+++ index.js
@@ -45,17 +45,22 @@ const coinValues = {
 	Pennies: 1,
 };
 
-const coinOrder = ['Quarters', 'Dimes', 'Nickels', 'Pennies'];
+const coins = [
+	{ name: 'Quarters', value: 25 },
+	{ name: 'Dimes', value: 10 },
+	{ name: 'Nickels', value: 5 },
+	{ name: 'Pennies', value: 1 },
+];
 
 const diff = payment - itemCost;
 let change = diff;
 let total = 0;
 
-for (let coin of coinOrder) {
-	const coinQuantity = Math.floor(change / coinValues[coin]);
-	total += coinQuantity * coinValues[coin];
-	const remainder = change % coinValues[coin];
-	if (coinQuantity) console.log(`${coin}: ${coinQuantity}`);
+for (let coin of coins) {
+	const coinQuantity = Math.floor(change / coin.value);
+	total += coinQuantity * coin.value;
+	const remainder = change % coin.value;
+	if (coinQuantity) console.log(`${coin.name}: ${coinQuantity}`);
 	if (!remainder) break;
 	else change = remainder;
 }
